de5b747021e9620d1cb2e4c8964d983247f168ae
Nick Watts <nick@nawatts.com>
2012-04-13 14:43:36 -0400

    Asynchronously fetch new landmark data after large position changes.

--- Reveal/RVLandmarkManager.m
+++ Reveal/RVLandmarkManager.m
@@ -11,15 +11,18 @@
 #define kDefaultVisibleDistance 1000
 #define kVisibleDistanceUpdateFraction 0.25
 
+#define kLandmarkJSONSource @"http://localhost/~nwatts/reveal-editor/landmarks.php"
+
 #define DEG2RAD(angle) ((angle) / 180.0 * M_PI)
 
 @interface RVLandmarkManager ()
-
+@property (strong, nonatomic) NSMutableDictionary* landmark_cache;
 @end
 
 @implementation RVLandmarkManager
 
 @synthesize visible_distance = _visible_distance;
+@synthesize landmark_cache = _landmark_cache;
 
 - (void)dealloc
 {
@@ -51,9 +54,35 @@ double haversineDistance(CLLocationCoordinate2D c1, CLLocationCoordinate2D c2)
   return c * earth_radius;
 }
 
+#pragma mark - Update landmarks methods
+
+- (void)didFetchLandmarkData:(NSDictionary*)landmark_data
+{
+  NSLog(@"%@", landmark_data);
+}
+
+/* Asynchronously fetch landmark data from the JSON source specified in kLandmarkJSONSource
+ * Display an alert if there is an error retrieving or parsing the data
+ * If no error, call didFetchLandmarkData with retrieved data
+ */
 - (void)updateLandmarkCache
 {
-  NSLog(@"Update Landmarks");
+  [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:kLandmarkJSONSource]] 
+                                     queue:[[[NSOperationQueue alloc] init] autorelease]
+                         completionHandler:^(NSURLResponse *response, NSData *data, NSError *error){
+                           if ( ! error ) {
+                             NSDictionary* landmark_json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+                             if ( ! error ) {
+                               [self didFetchLandmarkData:landmark_json];
+                             } else {
+                               UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"Error" message:error.description delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil] autorelease];
+                               [alert show];
+                             }
+                           } else {
+                             UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"Error" message:error.description delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil] autorelease];
+                             [alert show];
+                           }
+                         }];
 }
 
 #pragma mark - Notification Observers
